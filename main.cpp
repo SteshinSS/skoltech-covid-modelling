51731ffb43a3cc393fc2db8e7d9f0274e867041b
me <poft@yandex.ru>
2020-08-01 13:17:49 +0300

    Finish the draft of the modeller

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <math.h>
+#include <random>
 
 struct Human {
     double x;
@@ -14,16 +15,26 @@ double GetDistance(const Human &lhs, const Human &rhs) {
 
 std::vector<Human> GenerateQueue(double social_distance, int size, double infected_fraction) {
     std::vector<Human> queue;
+
+    std::random_device rd{};
+    std::mt19937 rng{rd()};
+    std::bernoulli_distribution distribution(infected_fraction);
+
+
     double x = 0.0;
     for (int i = 0; i < size; ++i) {
-        queue.push_back(Human{x, 0.0});
+        queue.push_back(Human{x, 0.0, distribution(rng)});
         x += social_distance;
     }
     return queue;
 }
 
-std::vector<Human> GenerateCrowd(double social_distance, int size) {
+std::vector<Human> GenerateCrowd(double social_distance, int size, double infected_fraction) {
     std::vector<Human> result;
+
+    std::random_device rd{};
+    std::mt19937 rng{rd()};
+    std::bernoulli_distribution distribution(infected_fraction);
 
     // Generate crowd as a square grid
     // So if it's side equal N, then there will be NxN people
@@ -31,20 +42,20 @@ std::vector<Human> GenerateCrowd(double social_distance, int size) {
 
     for (int i = 0; i < side; ++i) {
         for (int j = 0; j < side; ++j) {
-            result.push_back(Human{i*social_distance, j*social_distance});
+            result.push_back(Human{i*social_distance, j*social_distance, distribution(rng)});
         }
     }
 
     // We generated side x side people. Let's add what left
     int total_left = size - side * side;
     for (int i = 0; i < total_left; ++i) {
-        result.push_back(Human{side*social_distance, (side + i)*social_distance});
+        result.push_back(Human{side*social_distance, (side + i)*social_distance, distribution(rng)});
     }
 
     return result;
 }
 
-double ScoreFunction1(double distance) {
+double ScoreFunctionFirst(double distance) {
     if (distance > 5.0) {
         return 0;
     }
@@ -73,6 +84,56 @@ double CalculateScore(const std::vector<Human> &people, Func ScoreFunction) {
 }
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    do {
+        std::cout << "Choose type of a crowd." << std::endl;
+        std::cout << "Enter \'G\' for grid" << std::endl;
+        std::cout << "Enter \'Q\' for queue" << std::endl;
+        std::cout << "> ";
+        char crowd_type;
+        std::cin >> crowd_type;
+        if (crowd_type != 'Q' && crowd_type != 'G') {
+            std::cout << "Please enter Q or G." << std::endl;
+            continue;
+        }
+
+        std::cout << "Enter number of people" << std::endl;
+        std::cout << "> ";
+        int size;
+        std::cin >> size;
+
+        std::cout << "Enter average distance between people" << std::endl;
+        if (crowd_type == 'G') {
+            std::cout << "In case of grid crowd, enter length of grid's cell" << std::endl;
+        }
+        std::cout << "> ";
+        double distance;
+        std::cin >> distance;
+
+        std::cout << "Enter fraction of already infected people" << std::endl;
+        std::cout << "> ";
+        double infected_fraction;
+        std::cin >> infected_fraction;
+
+        std::cout << std::endl;
+        std::cout << "Calculating score..." << std::endl;
+
+        std::vector<Human> crowd;
+        if (crowd_type == 'G') {
+            crowd = GenerateCrowd(distance, size, infected_fraction);
+        } else if (crowd_type == 'Q') {
+            crowd = GenerateQueue(distance, size, infected_fraction);
+        }
+
+        double score = CalculateScore(crowd, ScoreFunctionFirst);
+        std::cout << "Infection score: " << score << std::endl << std::endl;
+        std::cout << "Change parameters? (Y/N)" << std::endl;
+        std::cout << "> " << std::endl;
+        char change_parameters;
+        std::cin >> change_parameters;
+        if (change_parameters != 'Y') {
+            break;
+        }
+    } while (true);
+
     return 0;
 }
